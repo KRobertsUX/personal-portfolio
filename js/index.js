662932c2224cfd166cfed503e91f55b1c231389f
Katarina Roberts <katarinarobertsux@gmail.com>
2021-07-27 14:42:31 +0100

    Update index.js
    atom beautify

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-function menuToggle () {
-    var x = document.getElementById('myNavtoggle')
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive'
-    } else {
-      x.className = 'navtoggle'
-    }
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
   }
-  
-  function sectionShowHide () {
-    var x = document.getElementById('showHideId')
-    if (x.className === 'showHide') {
-      x.className += ' responsiveSection'
-    } else {
-      x.className = 'showHide'
-    }
+}
+
+function sectionShowHide() {
+  var x = document.getElementById('showHideId')
+  if (x.className === 'showHide') {
+    x.className += ' responsiveSection'
+  } else {
+    x.className = 'showHide'
   }
+}
